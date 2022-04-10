4c0acc27981933f26465b0be3aaa3548c60b4868
Ana <lipcanana1997@gmail.com>
2022-04-11 00:14:05 +0300

    Add first project dependencies and define the WebdDriverManager

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,2 +1,23 @@
+import managers.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestRunner {
+    public static void main(String[] args) {
+
+//        WebDriverManager FfDriverManager = new WebDriverManager("firefox");
+//        FfDriverManager.getDriver().get("https://www.amazon.com/");
+//        FfDriverManager.getDriver().close();
+//        FfDriverManager.getDriver().quit();
+
+//        WebDriverManager ChDriverManager = new WebDriverManager("chrome");
+//        ChDriverManager.getDriver().get("https://www.google.com/");
+//        ChDriverManager.getDriver().close();
+//        ChDriverManager.getDriver().quit();
+
+        WebDriverManager EgDriverManager = new WebDriverManager("Edge");
+        EgDriverManager.getDriver().get("https://www.msn.com/en-xl/");
+        EgDriverManager.getDriver().close();
+        EgDriverManager.getDriver().quit();
+    }
 }
